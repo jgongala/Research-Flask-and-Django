4f15a2483681742c913332c4531b8e285136288c
Julia Gongala <65823190+jgongala@users.noreply.github.com>
2024-02-16 07:59:45 +0000

    Add files via upload

--- Django/djangoBlog/blog/urls.py
+++ Django/djangoBlog/blog/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('home/', views.home, name="home"),
     path('register/', views.register, name="register"),
+    path('logout/', views.user_logout, name="logout"),
+    path('createPost/', views.createPost, name="createPost"),
+    path('delete/<int:post_id>/', views.delete, name="delete"),
+    path('edit/<int:post_id>/', views.edit, name="edit")
 ]
--- Django/djangoBlog/blog/views.py
+++ Django/djangoBlog/blog/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render, redirect
-from .forms import RegistrationForm
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import RegistrationForm, PostForm
+from django.contrib import messages
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from .models import Post
 
-# Create your views here.
+@login_required(login_url="/login")
 def home(request):
-    return render(request, 'home.html')
+    # Retrieve all posts from the database
+    posts = Post.objects.all() 
+    
+    return render(request, 'home.html', {'posts': posts})
 
 def register(request):
     if request.method == 'POST':
@@ -20,7 +26,63 @@ def register(request):
 
     return render(request, 'registration/register.html', {'form': form})
 
-# def user_logout(request):
-#     logout(request)
-#     return redirect('home')  
+def user_logout(request):
+    logout(request)
+    return redirect('home')  
 
+@login_required(login_url="/login")
+def createPost(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect("/home")
+        
+    else:
+        form = PostForm()
+        
+    return render(request, 'createPost.html', {"form": form})
+
+def delete(request, post_id):
+    # Get the post from the database or return a 404 error if not found
+    post = get_object_or_404(Post, id=post_id)
+
+    # Check if the current user is the author of the post
+    if request.user.is_authenticated and request.user == post.author:
+        # Delete the post from the database
+        post.delete()
+
+        # Display a success message
+        messages.success(request, 'Post deleted successfully!')
+    else:
+        # Display an error message if the user is not the author
+        messages.error(request, 'You do not have permission to delete this post.')
+
+    # Redirect to the home page or any other desired page
+    return redirect('home')
+
+@login_required(login_url="/login")
+def edit(request, post_id):
+    # Retrieve the post from the database or return a 404 error
+    post = get_object_or_404(Post, id=post_id)
+
+    # Check if the current user has permission to edit the post
+    if request.user != post.author:
+        messages.error(request, 'You do not have permission to edit this post.')
+        return redirect('home')
+
+    if request.method == 'POST':
+        # Handle form submission
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Post updated successfully!')
+            return redirect('home')
+    else:
+        # Pre-populate the form with the data from the post being edited
+        form = PostForm(instance=post)
+
+    return render(request, 'editPost.html', {"form": form, "post": post})
