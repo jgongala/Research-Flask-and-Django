5b5f3f69960410841b3be970c02f7db2faef066c
Julia Gongala <65823190+jgongala@users.noreply.github.com>
2024-03-27 09:53:25 +0000

    Delete advancedBlog directory

--- advancedBlog/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import UserRegister, ChangePassword, UserProfile
-
-urlpatterns = [
-    path('register/', UserRegister.as_view(), name='register'),
-    path('password/', ChangePassword.as_view(), name='passwordChange'),
-    path('editProfile/', UserProfile.as_view(), name='editProfile'),
-]
